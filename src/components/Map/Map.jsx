1ea8feb4381e8da29373e72ac80e57c70fa97d36
Elharti <AElharti@lims.com>
2023-01-16 12:25:04 +0100

    Adding the header and layout

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import googleMapReact from "google-map-react";
+import { Paper, Typofraphy, useMediaQuery } from "@material-ui/core";
+import { LocationCityOutlined } from "@material-ui/icons";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./style.js";
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
   return <div>Map</div>;
 };
 
